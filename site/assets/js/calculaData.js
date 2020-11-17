dceaa90253020b380bfd6c2100dfd04583aad2f0
Matheus Alves Nunes <matheus.alnun@gmail.com>
2020-11-17 16:14:29 -0300

    New ui

---
+++ site/assets/js/calculaData.js
@@ -0,0 +1,21 @@
+import decimo from './decimo.js';
+import dateParser from './dateParser.js'
+
+(function calculaData () {
+  document.getElementById('button').addEventListener('mouseup', (e) => {
+    const initial = document.getElementById('initial').value
+    const final = document.getElementById('final').value
+    const year  = document.getElementById('year').value
+
+    console.log(initial, final)
+
+    const result = decimo(dateParser(initial), dateParser(final), 
+    year !== ''? Number(year) : undefined);
+
+    console.log(result)
+
+    const response = document.getElementById('resposta');
+    response.innerHTML = '';
+    response.innerHTML = `${result} avo(s)`;
+  })
+})()
